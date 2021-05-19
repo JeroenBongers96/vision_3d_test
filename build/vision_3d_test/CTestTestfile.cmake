55054209dd660ac5771621f7d115505af55a713e
Robohub <info@robohub-eindhoven.nl>
2021-05-19 20:03:42 +0200

    cleanup: build folder

--- build/vision_3d_test/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/robohub/dev_ws/src/vision_3d_test
-# Build directory: /home/robohub/dev_ws/src/vision_3d_test/build/vision_3d_test
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
